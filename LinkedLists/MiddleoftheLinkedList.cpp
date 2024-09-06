df51079aa997a601832eacf68257caf0d21cb3e8
shankaer <ashanker15@gmail.com>
2024-09-07 03:21:34 +0530

    Update MiddleoftheLinkedList.cpp

--- LinkedLists/MiddleoftheLinkedList.cpp
+++ LinkedLists/MiddleoftheLinkedList.cpp
@@ -11,37 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        // fast pointer will move 2 steps , slow will move one
-        ListNode *slow = head ;
-        ListNode *fast = head ;
         
-        while( true ){
-            
-            
-            ListNode *fNext = fast->next  ;
-            ListNode *ffNext = ( fNext != nullptr ? fNext-> next : nullptr) ;
-            
-            if( fNext != nullptr && ffNext != nullptr ){  // middle of the list 
-                fast = ffNext ;
-                slow = slow->next ;
-            }
-            else if ( fNext != nullptr && ffNext == nullptr ){ // case will arise when fast is at second last node in case of even length list 
-                    
-                
-                slow = slow -> next ; // slow takes one step , ensures second middle 
-                fast = ffNext ;       // fast at last null node 
-                break ;
-            }
-            else if( fNext == nullptr ) { // fast at last node in case odd length list we have arrived at last and slow contains the answer 
-                
-                break ;
-                
-            }
-            
-            
-        }
+       ListNode *p = head ;
+       ListNode *q = head ;
+    while (q!= nullptr && q->next != nullptr){
         
-        return slow ;
-        
+        p=p->next ;
+        q= q->next->next ;
+    }
+     return p;   
     }
 };
